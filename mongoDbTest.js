67eefa2c759ec4fbeacdb18974f4adf8c5bdcc20
nokko <13428215+nok-ko@users.noreply.github.com>
2022-05-03 18:44:10 -0700

    Add command-line arguments, put all the database connection code in one place.

--- mongoDbTest.js
+++ mongoDbTest.js
@@ -2,12 +2,83 @@ import { MongoClient } from 'mongodb';
 import express from 'express';
 import fs from 'fs';
 
+// Use `yargs` to parse command-line arguments.
+import yargs from 'yargs'
+import { hideBin } from 'yargs/helpers'
+const argv = yargs(hideBin(process.argv))
+  .option('instanceAddress', {
+    alias: 'i',
+    description: "The IP address or hostname of the MongoDB instance to connect the app to. Defaults to `localhost`.",
+    type: 'string'
+  })
+  .option('instancePort', {
+    alias: 'p',
+    description: "The port number that the MongoDB instance listens on. Defaults to `27017`.",
+    type: 'number'
+  })
+  .option('dbName', {
+	  alias: 'db',
+	  description: "The name of the database to use in the MongoDB instance. Defaults to `test`.",
+	  type: 'string'
+  })
+  .help()
+  .alias('help', 'h').parse();
+
 const app = express();
-const url = "mongodb://localhost:27017";
-const dbName = "test";
+
+// Defaults for address and port:
+const url = `mongodb://${argv.instanceAddress ?? 'localhost'}:${argv.instancePort ?? '27017'}`;
+
+const dbName = argv.dbName ?? "test";
+
+// The MongoDB connection and Database objects.
+// Populated by the connectMongo(…) function.
+let mongo, db;
+
+// Establishes connection and returns necessary objects to interact with database
+// Caller is resonsible for closing the connection
+// TODO:
+// * Do this exactly once when the app starts up, save the connection.
+const connectMongo = async (url, dbName) => {
+	let dotAnim; // Interval handler for the "dots" animation.
+	try {
+		dotAnim = setInterval(() => process.stdout.write('…'), 1000);
+		const mongo = await Promise.race([
+			MongoClient.connect(url, {
+				useNewUrlParser: true,
+				useUnifiedTopology: true,
+			}),
+			new Promise((res,rej) => {
+				setTimeout(() => rej("Timed out after 15 seconds."), 15000)
+			})
+		]);
+		const db = mongo.db(dbName);
+		console.error(`Connected to "${url}", using database "${argv.dbName}"`);
+	} catch (error) {
+		console.log(); // Newline!
+		console.error(`Tried connecting to ${url}, using database ${dbName}`);
+		console.error("Ran into an error while connecting to MongoDB!");
+		console.error('Error object:');
+		console.dir(error);
+		console.error('Exiting early due to errors!')
+		// TODO: consider any cleanup code before exiting: any open file handles?
+		process.exit();
+	} finally {
+		clearInterval(dotAnim);
+	}
+	return [mongo, db];
+}
+
+console.log("Connecting to MongoDB instance…");
+try {
+	[mongo, db] = await connectMongo(url, dbName);
+} catch (err) {
+	console.error(`Could not connect to MongoDB instance at ${url}!`);
+}
+
+
 
 app.use(express.json());
-
 // static path mappings
 app.use("/js", express.static("public/js"));
 app.use("/css", express.static("public/css"));
@@ -21,8 +92,6 @@ app.get('/', function (req, res) {
 
 // Create example 
 app.post('/create-person', async (req, res) => {
-	// Establish connection
-	const [mongo, db] = await connectMongo();
 	console.log(req.body);
 	const name = req.body.name;
 	const email = req.body.email;
@@ -40,18 +109,12 @@ app.post('/create-person', async (req, res) => {
 		//send fail response
 		res.send({ status: "fail", msg: "could not add person." });
 	} 
-
-	// Close Connection
-	await mongo.close();
 })
 
 // Read example
 // This example just gets all the documents in a collection
 // use .fineOne() instead to get just one document
 app.get('/people', async function (req, res) {
-	// Establish connection
-	const [mongo, db] = await connectMongo();
-
 	// Get every document in 'testCollection' collection
 	// send names to client
   	const results = await db.collection('testCollection').find({}).toArray();
@@ -61,15 +124,10 @@ app.get('/people', async function (req, res) {
 
 	// Send query result
 	res.send(names);
-
-	// Close Connection
-	await mongo.close();
 })
 
 // Update example
 app.post('/change-name', async (req, res) => {
-	// Establish connection
-	const [mongo, db] = await connectMongo();
 	const givenEmail = req.body.email;
 	const newName = req.body.name;
 
@@ -85,16 +143,11 @@ app.post('/change-name', async (req, res) => {
 		console.log(e);
 		//send fail response
 		res.send({ status: "fail", msg: "could not change name." });
-	} 
-
-	// Close Connection
-	await mongo.close();
+	}
 })
 
 // Delete example
 app.post('/delete-person', async (req, res) => {
-	// Establish connection
-	const [mongo, db] = await connectMongo();
 	const email = req.body.email;
 	const name = req.body.name;
 	let errMsg = null;
@@ -113,25 +166,15 @@ app.post('/delete-person', async (req, res) => {
 		console.log(e);
 		//send fail response
 		res.send({ status: "fail", msg: errMsg ? errMsg : "could not delete person." });
-	} 
-
-	// Close Connection
-	await mongo.close();
+	}
 })
+
+
+
+
 
 // RUN SERVER
 let port = 8000;
 app.listen(port, function () {
   console.log('Listening on port ' + port + '!');
 })
-
-// Establishes connection and returns necessary objects to interact with database
-// Caller is resonsible for closing the connection
-const connectMongo = async() => {
-	const mongo = await MongoClient.connect(url, {
-		useNewUrlParser: true,
-		useUnifiedTopology: true,
-	});
-	const db = mongo.db(dbName);
-	return [mongo, db];
-}
