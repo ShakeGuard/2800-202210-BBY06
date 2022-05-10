16638ae7c88441587d29669057cedc742fc378b6
Katy Petrova <13428215+nok-ko@users.noreply.github.com>
2022-05-09 20:27:49 -0700

    Refresh dashboard users list on page load.

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -6,14 +6,63 @@ const userListEl = document.getElementById("user-list");
 /** @type HTMLTemplateElement */
 const userRowTemplate = document.getElementById("user-row");
 
-// Deep copy the contents of the template
+// Some JSDoc Record Types:
+/**
+ * @typedef {
+ *     {
+ *     "$date": {
+ *     "$numberLong": string
+ *     } 
+ * }} MongoDate
+ * @typedef {{
+ *      _id: string,
+ *      name: string,
+ *      emailAddress: string,
+ *      avatarURL: string,
+ *      dateJoined: MongoDate,
+ *      achievements: Array<string>,
+ *      admin: boolean
+ *      }} UserDoc
+ */
 
-/** @type HTMLDivElement */
-const exampleRow = userRowTemplate.content.cloneNode(true);
+/**
+ * Creates a <li> element representing an input UserDoc record.
+ * @param {UserDoc} userDoc 
+ * @returns {HTMLLIElement}
+ */
 
-/** @type HTMLParagraphElement */
-const nameParagraphEl = exampleRow.getElementById("admin-name");
+function makeUserRow(userDoc) {
+    /** @type HTMLLIElement */
+    // Deep copy the contents of the template:
+    const row = userRowTemplate.content.cloneNode(true).firstElementChild;
+    row.querySelector("#admin-name").textContent = userDoc.name;
+    row.setAttribute('data-user-id', userDoc._id);
+    // Fill in other stuff, too?
+    // TODO: Profile pictures!
+    return row;
+}
 
-nameParagraphEl.textContent = "Tracy Ly";
+// TODO: Pagination?
+/**
+ * Replace the children of the input user list with a fresh batch of <li> elements 
+ * generated from the result of the input fetch() Promise.
+ * @param {Promise<Response>} toFetch - Promise, usually from fetch(…).
+ * @returns {Number} - how many records were inserted into the list.
+ */
+async function refreshUsers(toFetch) {
+    // TODO: Error handling!
+    /** @type {Array<UserDoc>} */
+    const results = (await (await toFetch).json()).result;
 
-userListEl.appendChild(exampleRow);
+    console.dir(
+        results.map(makeUserRow).map(el => el.toString())
+    )
+
+    userListEl.replaceChildren(
+        ...results.map(makeUserRow)
+    )
+
+    return results.length;
+}
+
+refreshUsers(fetch('profiles'));
