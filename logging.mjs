1f6acb400cb01742a3e0d2b4d750f5264fdf5e1a
Katy Petrova <13428215+nok-ko@users.noreply.github.com>
2022-05-16 11:28:00 -0700

    Don't overwrite old log files anymore.
    Also adds a "Server started at …" message to logs before HTTP logging
    begins.

--- logging.mjs
+++ logging.mjs
@@ -1,5 +1,6 @@
 import morgan from 'morgan';
 import fs from 'node:fs';
+import { appendFile } from 'node:fs/promises';
 import path from 'node:path';
 
 /** Developer-use log to standard out. */
@@ -8,8 +9,12 @@ export const stdoutLog = morgan('dev');
 /** Error-only log, skips all responses that are successes or redirects. */
 export const errorLog = morgan('dev', {skip: (req, res) => res.statusCode < 400});
 
+// Before creating the filestream, quickly write a "Server Restarted!" message so it's easier to read.
+const logFileName = path.join(process.cwd(), '/logs/access.log');
+await appendFile(logFileName, `Server started at ${new Date().toLocaleString('en-CA', {timeZone: 'America/Vancouver'})}\n`);
+
 /** Append-only filestream for logs. */
-export const accessLogStream = fs.createWriteStream(path.join(process.cwd(), '/logs/access.log'));
+export const accessLogStream = fs.createWriteStream(logFileName, {flags: 'a'});
 
 /** Access log, appends to a logfile. */
 export const accessLog = morgan('combined', { stream: accessLogStream });
