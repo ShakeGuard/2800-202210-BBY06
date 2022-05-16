6c2ba6ddcfb8a0ce8f6f63e31c070a69c2d83514
Katy Petrova <13428215+nok-ko@users.noreply.github.com>
2022-05-16 12:21:16 -0700

    Minor improvement to comments in `logging.mjs`

--- logging.mjs
+++ logging.mjs
@@ -7,8 +7,6 @@ import path from 'node:path';
 /** Developer-use log to standard out. */
 export const stdoutLog = morgan('dev');
 
-/** Error-only log, skips all responses that are successes or redirects. */
-export const errorLog = morgan('dev', { skip: (req, res) => res.statusCode < 400 });
 
 // Before creating the filestream, quickly write a "Server Restarted!" message so it's easier to read.
 const logFileName = path.join(process.cwd(), '/logs/access.log');
@@ -17,9 +15,15 @@ await appendFile(logFileName, `Server started at ${new Date().toLocaleString('en
 /** Append-only filestream for logs. */
 export const accessLogStream = fs.createWriteStream(logFileName, {flags: 'a'});
 
-/** Access log, appends to a logfile. */
+/** Morgan access log, appends to a logfile. */
 export const accessLog = morgan('combined', { stream: accessLogStream });
 
+/** Morgan error-only log, skips all responses that are successes or redirects. */
+export const errorLog = morgan('dev', { skip: (req, res) => res.statusCode < 400 });
+
+/** Main log object, used more or less like `console.whatever()`.
+ * @type {winston.Logger}
+*/
 export const log = winston.createLogger({
     level: 'info',
     format: winston.format.json(),
@@ -30,6 +34,7 @@ export const log = winston.createLogger({
 });
 /**
  * Adds log output to standard out, as well as the default file transports.
+ * @param {winston.Logger} log - logger to add the devLog stuff to
  */
 export function addDevLog(log) {
     log.add(
