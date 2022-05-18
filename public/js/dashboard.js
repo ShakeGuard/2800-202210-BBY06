29c3fe098305e253712e452b7bd1ae78b8efce32
Tracy Ly <tly35@my.bcit.ca>
2022-05-17 21:08:34 -0700

    Fix avatars not loading

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -51,9 +51,8 @@ function makeUserRow(userDoc) {
     // The profile pictures
     const userAvatar = row.getElementsByClassName('profile-picture').item(0);
     if (userDoc.avatar !== defaultAvatar && userDoc.avatar !== null) {
-        userAvatar.setAttribute('data', userDoc.avatar);
-        let base64 = userDoc.avatar.data;
-        base64 = `data:${userDoc.avatar.mimeType};base64,${base64}`;
+        let base64 = userDoc.avatar.data['$binary'].base64;
+        base64 = `data:${userDoc.avatar.contentType};base64,${base64}`;
         userAvatar.src = base64;
     }
 
